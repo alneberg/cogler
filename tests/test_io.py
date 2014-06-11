af755694bd6e1e96181af354f0121c0a19083e47
Johannes Alneberg <johannesalneberg@gmail.com>
2014-06-11 17:22:45 +0200

    Tests for io

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 from nose.tools import assert_equal, assert_true, assert_almost_equal, nottest
+import nose.plugins.skip as skip
 from os.path import isdir,isfile
 from os import listdir
 import os
 import sys
 
-from cogler.io import read_blast_output, read_markers_file, read_clustering_file
+from cogler.io import (read_blast_output, read_markers_file,
+    read_clustering_file, read_per_phylum_scgs)
 
 file_path = os.path.realpath(__file__)
 test_dir_path = os.path.dirname(file_path)
@@ -15,6 +17,7 @@ class TestIO(object):
     """Test input output for cogler"""
 
     def test_read_per_phylum_scgs(self):
+        """Test for the read_per_phylum_scgs method."""
         summary, phyla_scgs = read_per_phylum_scgs(
                 os.path.join(test_data_path, "test_phyla_scg.tsv"))
         # First row
@@ -41,7 +44,9 @@ class TestIO(object):
 
         assert_true(phyla_scgs.ix['Acidobacteria'].sum() == summary.ix['Acidobacteria'].Number_SCG)
         assert_true(phyla_scgs.ix['Gammaproteobacteria'].sum() == summary.ix['Gammaproteobacteria'].Number_SCG)
+
     def test_blast(self):
+        """Test for read_blast_output method."""
         records, sseq_ids = read_blast_output(
                 os.path.join(test_data_path, "test_contigs.out"))
         assert_true(len(records) > 0)
@@ -69,15 +74,29 @@ class TestIO(object):
         assert_true(first_record['slen'] == 176)
 
     def test_markers_file(self):
+        """Test for read_markers_file method."""
         markers = read_markers_file(
                 os.path.join(test_data_path, "test_markers_file.txt"))
         assert_true(len(markers) == 36)
         assert_true(markers[0] == 'COG0016')
         assert_true(markers[-1] == 'COG0130')
 
+    def test_gff_file(self):
+        """Test for read_gff_file method."""
+        reason = "Test not written yet"
+        raise skip.SkipTest(reason)
+
     def test_clustering_file(self):
+        """Test for read_clustering_file method."""
         clusters, contigs_per_cluster = read_clustering_file(
                 os.path.join(test_data_path, "test_clustering_gt1000.csv"))
         assert_true(len(clusters) == 2)
         assert_true('47' in clusters)
         assert_true('94' in clusters)
+
+        assert_true(len(contigs_per_cluster['47']) == 7)
+        assert_true(len(contigs_per_cluster['94']) == 2)
+        assert_true('contig00001' in contigs_per_cluster['47'])
+        assert_true('contig00009' in contigs_per_cluster['47'])
+        assert_true('contig00004' in contigs_per_cluster['94'])
+        assert_true('contig00006' in contigs_per_cluster['94'])
