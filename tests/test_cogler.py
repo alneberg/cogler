73f8c620675abb44abecbbdb50b7e69e2c5b0750
Johannes Alneberg <johannesalneberg@gmail.com>
2014-06-12 16:30:49 +0200

    Corrected tests for the cogler/__init__.py methods

--- tests/test_cogler.py
+++ tests/test_cogler.py
@@ -5,8 +5,8 @@ from os.path import isdir,isfile
 from os import listdir
 import os
 import sys
-
-from cogler import CogsPerClusters, Phylum, Output
+import pandas as pd
+from cogler import CogsPerContig, Phylum, Output
 
 file_path = os.path.realpath(__file__)
 test_dir_path = os.path.dirname(file_path)
@@ -16,28 +16,50 @@ class TestCogsPerClusters(object):
     """Test the CogsPerClusters class for cogler"""
     
     def setUp(self):
-        self.contigs_per_cluster = {'1': ['contig1', 'contig3'],
-                '2': ['contig2']}
+        self.cluster_per_contig = {'contig1': '1',
+                                   'contig2': '2',
+                                   'contig3': '1'}
+
         self.features_per_contig = {'contig1': ['COG0001', 'COG0004'],                           
                                     'contig2': ['COG0002', 'COG0004'],
                                     'contig3': ['COG0003', 'COG0001']} 
 
-        self.cogs_per_clusters = CogsPerClusters(self.contigs_per_clusters, 
-                                                self.features_per_contig)
+        self.cogs_per_contig = CogsPerContig(self.cluster_per_contig,
+                                             self.features_per_contig)
     
     def test_init(self):
-        """Test for the CogPerCluster init method."""
-        assert_true(self.cogs_per_clusters.contigs_per_cluster == self.contigs_per_cluster)
-        assert_true(self.cogs_per_clusters.features_per_contig == self.features_per_contig)
+        """Test for the CogsPerContig init method."""
+        assert_true(self.cogs_per_contig.cluster_per_contig == self.cluster_per_contig)
+        assert_true(self.cogs_per_contig.features_per_contig == self.features_per_contig)
 
-    def test_count_features_per_cluster(self):
-        """Test for count_features_per_cluster method."""
-        features_per_cluster = self.cogs_per_clusters.count_features_per_cluster()
+    def test_correct_values(self):
+        """Test that the values in CogPerContig are correct."""
+        assert_true(self.cogs_per_contig.df.ix['contig1','COG0001'] == 1)
+        assert_true(self.cogs_per_contig.df.ix['contig1','COG0004'] == 1)
+        assert_true(self.cogs_per_contig.df.ix['contig2','COG0002'] == 1)
+        assert_true(self.cogs_per_contig.df.ix['contig2','COG0004'] == 1)
+        assert_true(self.cogs_per_contig.df.ix['contig3','COG0003'] == 1)
+        assert_true(self.cogs_per_contig.df.ix['contig3','COG0001'] == 1)
 
-        assert_true(features_per_cluster['1'] == {'COG0001': 2, 'COG0003': 1, 'COG0004': 1})
-        assert_true(features_per_cluster['2'] == {'COG0002': 1, 'COG0004': '1'})
+        # The nulls
+        assert_true(pd.isnull(self.cogs_per_contig.df.ix['contig1','COG0002']))
+        assert_true(pd.isnull(self.cogs_per_contig.df.ix['contig1','COG0003']))
+        assert_true(pd.isnull(self.cogs_per_contig.df.ix['contig2','COG0001']))
+        assert_true(pd.isnull(self.cogs_per_contig.df.ix['contig2','COG0003']))
+        assert_true(pd.isnull(self.cogs_per_contig.df.ix['contig3','COG0002']))
+        assert_true(pd.isnull(self.cogs_per_contig.df.ix['contig3','COG0004']))
 
-        assert_true(len(features_per_clusters) == 2)
+    def test_cluster_membership(self):
+        """Assert the contigs have the correct cluster assignment."""
+        assert_true(self.cogs_per_contig.df.ix['contig1','cluster'] == '1')
+        assert_true(self.cogs_per_contig.df.ix['contig2','cluster'] == '2')
+        assert_true(self.cogs_per_contig.df.ix['contig3','cluster'] == '1')
+
+
+    def test_number_of_values(self):
+        """Assert nothing else is in the dataframe."""
+        assert_true(self.cogs_per_contig.df.shape == (3,5))
+
 
 class TestPhylum(object):
     """Test the Phylum class for cogler"""
@@ -53,70 +75,72 @@ class TestPhylum(object):
 class TestOutput(object):
     """Test the Output class for cogler"""
     def setUp(self):
-        self.contigs_per_cluster = {'1': ['contig1', 'contig3'],                        
-                '2': ['contig2']}
+        self.cluster_per_contig = {'contig1': '1',
+                                   'contig2': '2',
+                                   'contig3': '1'}
 
         self.features_per_contig = {'contig1': ['COG0001', 'COG0004'],
                                     'contig2': ['COG0002', 'COG0004'],
                                     'contig3': ['COG0003', 'COG0001']}
-        self.cogs_per_clusters = CogsPerClusters(self.contigs_per_clusters,
+        self.cogs_per_contig = CogsPerContig(self.cluster_per_contig,
                                                  self.features_per_contig)
 
-        self.phylum1 = Phylum("Phylum1", ['COG0001', 'COG00004'])
-        self.phylum2 = Phylum("Phylum2", ['COG0002', 'COG00003', 'COG0004'])
+        self.phylum1 = Phylum("Phylum1", ['COG0001', 'COG0004'])
+        self.phylum2 = Phylum("Phylum2", ['COG0002', 'COG0003', 'COG0004'])
         self.phylum3 = Phylum("Phylum3", ['COG0010', 'COG0004'])
 
     def test_simple(self):
         """A simple use case test for the Output class"""
-        output = Output(self.cogs_per_clusters)
+        output = Output(self.cogs_per_contig)
         output.add_phylum(self.phylum1)
-        assert_true(output.result_matrix.columns == ['cluster', 'Phylum1 total', 'Phylum1 >1', 'Phylum1 ==1'])
-        assert_true(output.result_matrix.cluster == ['1', '2'])
-        assert_true(output.result_matrix.ix[['1', 'Phylum1 total']] == 2)
-        assert_true(output.result_matrix.ix[['1', 'Phylum1 >1']] == 1)
-        assert_true(output.result_matrix.ix[['1', 'Phylum1 ==1']] == 1)
+        assert_true(set(output.result_matrix.columns) == set(['Phylum1 total', 'Phylum1 >1', 'Phylum1 ==1']))
+        assert_true(len(output.result_matrix.index.values) == 2)
+        assert_true(set(output.result_matrix.index.values) == set(['1', '2']))
+        assert_true(output.result_matrix.ix['1', 'Phylum1 total'] == 2)
+        assert_true(output.result_matrix.ix['1', 'Phylum1 >1'] == 1)
+        assert_true(output.result_matrix.ix['1', 'Phylum1 ==1'] == 1)
 
-        assert_true(output.result_matrix.ix[['2', 'Phylum1 total']] == 2)
-        assert_true(output.result_matrix.ix[['2', 'Phylum1 >1']] == 0)
-        assert_true(output.result_matrix.ix[['2', 'Phylum1 ==1']] == 1)
+        assert_true(output.result_matrix.ix['2', 'Phylum1 total'] == 2)
+        assert_true(output.result_matrix.ix['2', 'Phylum1 >1'] == 0)
+        assert_true(output.result_matrix.ix['2', 'Phylum1 ==1'] == 1)
 
     def test_all(self):
         """A more complex use case with three phyla for the Output class"""
-        output = Output(self.cogs_per_clusters)
+        output = Output(self.cogs_per_contig)
         output.add_phylum(self.phylum1)
         output.add_phylum(self.phylum2)
         output.add_phylum(self.phylum3)
 
-        columns = ['cluster', 'Phylum1 total', 'Phylum1 >1', 'Phylum1 ==1',
+        columns = ['Phylum1 total', 'Phylum1 >1', 'Phylum1 ==1',
                 'Phylum2 total', 'Phylum2 >1', 'Phylum2 ==1',
                 'Phylum3 total', 'Phylum3 >1', 'Phylum3 ==1']
 
         for column in columns:
             assert_true(column in output.result_matrix.columns)
+        assert_true(len(output.result_matrix.index.values) == 2)
+        assert_true(set(output.result_matrix.index.values) == set(['1', '2']))
+        assert_true(output.result_matrix.ix['1', 'Phylum1 total'] == 2)
+        assert_true(output.result_matrix.ix['1', 'Phylum1 >1'] == 1)
+        assert_true(output.result_matrix.ix['1', 'Phylum1 ==1'] == 1)
 
-        assert_true(output.result_matrix.cluster == ['1', '2'])
-        assert_true(output.result_matrix.ix[['1', 'Phylum1 total']] == 2)
-        assert_true(output.result_matrix.ix[['1', 'Phylum1 >1']] == 1)
-        assert_true(output.result_matrix.ix[['1', 'Phylum1 ==1']] == 1)
+        assert_true(output.result_matrix.ix['2', 'Phylum1 total'] == 2)
+        assert_true(output.result_matrix.ix['2', 'Phylum1 >1'] == 0)
+        assert_true(output.result_matrix.ix['2', 'Phylum1 ==1'] == 1)
 
-        assert_true(output.result_matrix.ix[['2', 'Phylum1 total']] == 2)
-        assert_true(output.result_matrix.ix[['2', 'Phylum1 >1']] == 0)
-        assert_true(output.result_matrix.ix[['2', 'Phylum1 ==1']] == 1)
+        assert_true(output.result_matrix.ix['1', 'Phylum2 total'] == 3)
+        assert_true(output.result_matrix.ix['1', 'Phylum2 >1'] == 0)
+        assert_true(output.result_matrix.ix['1', 'Phylum2 ==1'] == 2)
 
-        assert_true(output.result_matrix.ix[['1', 'Phylum2 total']] == 3)
-        assert_true(output.result_matrix.ix[['1', 'Phylum2 >1']] == 0)
-        assert_true(output.result_matrix.ix[['1', 'Phylum2 ==1']] == 2)
+        assert_true(output.result_matrix.ix['2', 'Phylum2 total'] == 3)
+        assert_true(output.result_matrix.ix['2', 'Phylum2 >1'] == 0)
+        assert_true(output.result_matrix.ix['2', 'Phylum2 ==1'] == 2)
 
-        assert_true(output.result_matrix.ix[['2', 'Phylum2 total']] == 3)
-        assert_true(output.result_matrix.ix[['2', 'Phylum2 >1']] == 0)
-        assert_true(output.result_matrix.ix[['2', 'Phylum2 ==1']] == 2)
-
-        assert_true(output.result_matrix.ix[['1', 'Phylum3 total']] == 2)
-        assert_true(output.result_matrix.ix[['1', 'Phylum3 >1']] == 0)
-        assert_true(output.result_matrix.ix[['1', 'Phylum3 ==1']] == 1)
+        assert_true(output.result_matrix.ix['1', 'Phylum3 total'] == 2)
+        assert_true(output.result_matrix.ix['1', 'Phylum3 >1'] == 0)
+        assert_true(output.result_matrix.ix['1', 'Phylum3 ==1'] == 1)
         
-        assert_true(output.result_matrix.ix[['2', 'Phylum3 total']] == 2)
-        assert_true(output.result_matrix.ix[['2', 'Phylum3 >1']] == 0)
-        assert_true(output.result_matrix.ix[['2', 'Phylum3 ==1']] == 1)
+        assert_true(output.result_matrix.ix['2', 'Phylum3 total'] == 2)
+        assert_true(output.result_matrix.ix['2', 'Phylum3 >1'] == 0)
+        assert_true(output.result_matrix.ix['2', 'Phylum3 ==1'] == 1)
 
 
